1dd59a18a8c94115fb4dd6f8a3688619443459a7
Lucas Lange <58046434+lucasmullerlange@users.noreply.github.com>
2020-07-28 16:17:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.green,
+          primarySwatch: Colors.cyan,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         routes: {
